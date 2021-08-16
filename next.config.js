c244c989a0a52aaec4c16017adb324fcd83da508
AmauriMarcos <amaurisantos.m@gmail.com>
2021-08-16 12:58:58 +0200

    change next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    USER_ID: 'user_JL9i8mZlbV5ymPj0oB52b',
-    TOKEN: '3f5b9baf2350fa676eddecacc61df10e'
+    USER_ID: 'test',
+    TOKEN: 'test1'
   },
 }
