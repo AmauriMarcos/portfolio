6f02fef1620238cfe6e1f714e24440c7d51c804a
AmauriMarcos <amaurisantos.m@gmail.com>
2021-08-31 00:49:00 +0200

    add head viewport to app js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,22 @@
-import Layout from '../components/Layout';
-import '../styles/global.scss';
-import Head from 'next/head'
+import Layout from "../components/Layout";
+import "../styles/global.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-          <meta name="viewport" content="viewport-fit=cover" />
-          <title>Amauri Santos</title>
-        </Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+        <title>Amauri Santos</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
